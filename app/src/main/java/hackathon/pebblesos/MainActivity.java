2e50db5c522ca609b70689b16a2fc6bfa2bc2d3f
Daniel Ledlow <ledlowdaniel@gmail.com>
2015-09-06 12:13:57 -0400

    added apk and small ui update

--- app/src/main/java/hackathon/pebblesos/MainActivity.java
+++ app/src/main/java/hackathon/pebblesos/MainActivity.java
@@ -81,6 +81,8 @@ public class MainActivity extends ActionBarActivity {
                     TextView tv = (TextView)findViewById(R.id.text_messages);
                     tv.append("You: " + txt + "\n");
 
+                    textIn.setText("");
+
                     //Toast.makeText(context, txt, Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(context, "Watch is not connected!", Toast.LENGTH_LONG).show();
